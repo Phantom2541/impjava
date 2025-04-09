cb429d56d2bb7fa6883295f608942e1dd813f8d7
Phantom2541 <technowiz.thomas.iii.pajarillaga@gmail.com>
2025-04-09 09:20:23 +0800

    initial commit

---
+++ src/main/java/com/mycompany/impjava/Impjava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.impjava;
+
+/**
+ *
+ * @author Smart Care
+ */
+public class Impjava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
