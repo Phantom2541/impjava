9a2578708c234866e17eb820cab777184ec5959e
Phantom2541 <technowiz.thomas.iii.pajarillaga@gmail.com>
2025-04-09 12:03:22 +0800

    database

--- src/main/java/com/mycompany/impjava/Impjava.java
+++ src/main/java/com/mycompany/impjava/Impjava.java
@@ -4,6 +4,10 @@
 
 package com.mycompany.impjava;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 /**
  *
  * @author Smart Care
@@ -11,6 +15,22 @@ package com.mycompany.impjava;
 public class Impjava {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String url = "jdbc:mysql://localhost:3306/implibrary";
+        String user = "root";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, user, "");
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM books");
+            while(rs.next()){
+                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getString(4)+"  "+rs.getString(5));
+            }
+            con.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        // System.out.println("Hello World!");
     }
 }
